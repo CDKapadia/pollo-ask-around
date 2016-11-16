e901f5a295b1b606544c07a6ca5c92ca04fc6b49
Chiraag Kapadia <Chiraag@Chiraags-MacBook-Pro.local>
2016-11-15 19:37:26 -0600

    testing

---
+++ Pollo Ask Around/FavoritesViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FavoritesViewController.swift
+//  Pollo Ask Around
+//
+//  Created by Chiraag Kapadia on 11/15/16.
+//  Copyright © 2016 CAKK. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var storyboard = UIStoryboard(name: "Favorites", bundle: nil)
+        var controller = storyboard.instantiateInitialViewController()! as UIViewController
+        addChildViewController(controller)
+        view.addSubview(controller.view)
+        controller.didMove(toParentViewController: self)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
