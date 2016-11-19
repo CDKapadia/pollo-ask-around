e1df78b412d13e9f331c314561fa3fe8df0db986
Kerry Li <kerryli1997@gmail.com>
2016-11-19 23:36:32 +0000

    removed bytecote files and got init_user working

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -5,6 +5,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/final438'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 # needed to run views logic when only importing package name from the outside
 import server.views
