891c6d129cf524fbee90a399663c56165a228046
Labuser <Labuser@wtk316-24.cec.wustl.edu>
2016-11-20 01:38:14 -0600

    yoooo the "dictionary" thooooo

--- Pollo Ask Around/MakePollViewController.swift
+++ Pollo Ask Around/MakePollViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class MakePollViewController: UIViewController {
 
+    @IBOutlet weak var pollQuestion: UITextField!
+    
+    @IBOutlet weak var pollOption1: UITextField!
+    
+    @IBOutlet weak var pollOption2: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -20,11 +26,65 @@ class MakePollViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+     @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true,completion: nil)
+    }
     
+    @IBOutlet weak var pollPost: UIBarButtonItem!
+
+    @IBAction func postAction(_ sender: UIBarButtonItem) {
+        let question = pollQuestion.text!
+        let option1: String = pollOption1.text!
+        let option2: String = pollOption2.text!
+        let latitude = 38.648114
+        let longitude = -90.311554
+        let uuid = UIDevice.current.identifierForVendor!.uuidString
+        //let uuid = "hri1o2jd-uto1-74jd-pqjfoe1g0317"
+        let roption2 = option2.replacingOccurrences(of: "'", with: "\\'", options: .literal, range: nil)
+        var request = URLRequest(url: URL(string: "http://52.43.103.143:3456/post")!)
+        request.httpMethod = "POST"
+        let postString1 = "q="+question
+        let postString2 = "&lat="+String(latitude)+"&lng="+String(longitude)
+        let postString3 = "&uuid="+uuid
+        let postString4 = "&options=['"+option1+"','"+roption2+"']"
+        let postString = postString1 + postString2 + postString3 + postString4
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+        dismiss(animated: true, completion: nil)
+        
+        
+    /*
+    q - question
+    lat - latitude
+    lng - longitude
+    uuid -
+    options -
+    */
+    
+    }
+    
+    
+    
+   // MARK: - Navigation
 
     /*
-    // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
--- Pollo Ask Around/PollTableAndViewController.swift
+++ Pollo Ask Around/PollTableAndViewController.swift
@@ -8,33 +8,120 @@
 
 import UIKit
 import MapKit
+//import CoreLocation
 
 class PollTableAndViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var pollsTableView: UITableView!
     
     @IBOutlet weak var mapOnPollsView: MKMapView!
     
-    var myArray = ["Mary", "Jane", "Billy"]
+    var myArray = ["Mary", "Jane", "MaryJane"]
 
+    //var locManager: CLLocationManager!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       // /user
     
-        //let locManager = CLLocationManager()
+        let uuid = UIDevice.current.identifierForVendor!.uuidString
+        //let uuid = "1"
+        var request = URLRequest(url: URL(string: ("http://52.43.103.143:3456/user/"+uuid))!)
+        request.httpMethod = "GET"
+        let queue:OperationQueue = OperationQueue()
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler:{ (response: URLResponse?, data: Data?, error: Error?) -> Void in
+            
+            do {
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: []) as? Dictionary<String,AnyObject> {
+                    print("ASynchronous\(jsonResult)")
+                    if ((jsonResult["error"]) != nil){
+                        self.createUser(ID: uuid)
+                    }
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            
+            
+        })
+        var request2 = URLRequest(url: URL(string: ("http://52.43.103.143:3456/user/"+uuid+"/post"))!)
+        request2.httpMethod = "GET"
+        let queue2:OperationQueue = OperationQueue()
+        
+        NSURLConnection.sendAsynchronousRequest(request2, queue: queue2, completionHandler:{ (response: URLResponse?, data: Data?, error: Error?) -> Void in
+            
+            do {
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: []) as? Dictionary<String,AnyObject> {
+                    print("ASynchronous\(jsonResult)")
+                    //for key in jsonResult{
+                        //let stringKey = String(describing: key)
+                      //  let dict = jsonResult[stringKey]
+                        //let post = dict?["q"] as! String
+                        //self.myArray.append(post)
+                    //}
+                    //self.pollsTableView.dataSource = self
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            
+            
+        })
+    
+        //     let locManager = CLLocationManager()
         //locManager.requestAlwaysAuthorization()
-        //locManager.requestWhenInUseAuthorization()
+        //locManager.delegate = self
+        
+    //    locManager.requestAlwaysAuthorization()
+    //    locManager.requestWhenInUseAuthorization()
         var currentLocation: CLLocation!
         currentLocation = CLLocation(latitude: 38.648114, longitude: -90.311554)
         //        if( CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse ||
-        //            CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedAlways){
-        //            currentLocation = locManager.location
-        //            centerMapOnLocation(location: currentLocation)
-        //            
-        //        }
+          //          CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedAlways){
+            //        currentLocation = locManager.location
+              //      centerMapOnLocation(location: currentLocation)
+        
+                //}
         centerMapOnLocation(location: currentLocation)
-        pollsTableView.dataSource = self
+        //pollsTableView.dataSource = self
         print("I am here")
         // Do any additional setup after loading the view.
     }
+    
+    //private func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+     //   switch status {
+    //    case .notDetermined:
+  //          print("A")
+            // If status has not yet been determied, ask for authorization
+    //        manager.requestAlwaysAuthorization()
+    //        break
+    //    case .authorizedWhenInUse:
+            // If authorized when in use
+    //        print("B")
+//manager.startUpdatingLocation()
+     //       var currentLocation: CLLocation!
+     //       currentLocation = manager.location
+     //       centerMapOnLocation(location: currentLocation)
+    //        break
+   //     case .authorizedAlways:
+    //        print("C")
+            // If always authorized
+   //         manager.startUpdatingLocation()
+//var currentLocation: CLLocation!
+   //         currentLocation = manager.location
+    //        centerMapOnLocation(location: currentLocation)
+//break
+   //     case .restricted:
+   //         print("D")
+            // If restricted by e.g. parental controls. User can't enable Location Services
+//
+//        case .denied:
+  //          // If user denied your app access to Location Services, but can grant access from Settings.app
+            
+  //          break
+   //     }
+    //}
     
     let regionRadius: CLLocationDistance = 1000
     func centerMapOnLocation(location: CLLocation) {
@@ -49,12 +136,35 @@ class PollTableAndViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        print("in cellForRow at \(indexPath)")
+        //print("in cellForRow at \(indexPath)")
         let myCell = UITableViewCell(style: .default, reuseIdentifier: nil)
         myCell.textLabel!.text = myArray[indexPath.row]
         
         return myCell
         
+    }
+    func createUser(ID: String){
+        var request = URLRequest(url: URL(string: "http://52.43.103.143:3456/user")!)
+        request.httpMethod = "POST"
+        let postString = "uuid="+ID
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                // check for fundamental networking error
+                print("error=\(error)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
     }
 
 
