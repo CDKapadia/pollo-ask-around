b4c63fc4ddfcc23edb04514ef0a770451b8562cf
Kerry Li <kerryli1997@gmail.com>
2016-11-20 01:33:49 +0000

    first run at creating posts

--- server/server/models.py
+++ server/server/models.py
@@ -9,14 +9,53 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     __tablename__ = 'users'
-    uid = db.Column(db.Integer, primary_key=True)
-    uuid = db.Column(db.String(36), unique=True)
 
-    '''
-        id auto incremenents for db, uuid is the unique identifier for each device
-    '''
+    uid = db.Column(db.Integer, primary_key=True)
+
+    # unique device indentifier
+    uuid = db.Column(db.String(36), unique=True, nullable=False)
+
     def __init__(self, uuid):
         self.uuid = uuid
 
     def __repr__(self):
         return "<User: %s>" % self.uuid
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    pid = db.Column(db.Integer, primary_key=True)
+    lat = db.Column(db.DECIMAL, nullable=False)
+    lng = db.Column(db.DECIMAL, nullable=False)
+    question = db.Column(db.String(100), nullable=False)
+
+    # author of the post
+    uid = db.Column(db.Integer, db.ForeignKey('users.uid'), nullable=False)
+    author = db.relationship('User', backref=db.backref('posts', lazy='dynamic'))
+
+    def __init__(self, question, lat, lng, author):
+        self.question = question
+        self.lat = lat
+        self.lng = lng
+        self.author = author
+
+    def __repr__(self):
+        return '<Post: %s>' % self.question
+
+class Option(db.Model):
+    __tablename__ = "options"
+
+    oid = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(50), nullable=False)
+    votes = db.Column(db.Integer, nullable=False)
+
+    pid = db.Column(db.Integer, db.ForeignKey('posts.pid'), nullable=False)
+    post = db.relationship('Post', backref=db.backref('options', lazy='dynamic'))
+
+    def __init__(self, text, pid, votes=0):
+        self.text = text
+        self.pid = pid
+        self.votes = votes
+
+    def __repr__(self):
+        return '<Post Option: %i, %s, votes: %i>' % self.pid, self.text, self.votes
--- server/server/views.py
+++ server/server/views.py
@@ -2,28 +2,67 @@
     views file that defines behavior for each webppage
 '''
 import json
+import ast
 from flask import request
 from server import app
-from server.models import db, User
+from server.models import db, User, Post, Option
 import sqlalchemy.exc
 
 db.create_all()
+
 
 @app.route('/')
 def index():
     return "Hello, World!<br>Kevin sucks"
 
+
 @app.route('/init_user')
 def init_user():
     response = {}
     uuid = request.args.get('uuid')
-    user = User(uuid)
-    try:
-        db.session.add(user)
-        db.session.commit()
-        response['message'] = 'created'
-    except sqlalchemy.exc.IntegrityError:
-        response['message'] = 'uuid exists'
-    except:
-        response['message'] = 'db error'
+    if uuid is None:
+        response['message'] = 'no uuid provided'
+    else:
+        user = User(uuid)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            response['message'] = 'created'
+        except sqlalchemy.exc.IntegrityError:
+            response['message'] = 'uuid exists'
+        except:
+            response['message'] = 'db error'
     return json.dumps(response)
+
+
+@app.route('/create_post', methods=['POST'])
+def create_post():
+    required_args = ['question', 'lat', 'lng', 'uuid', 'options']
+    passed_params = {}
+    response = {}
+
+    for key in requiredargs:
+        passed_params[key] = request.form.get('key', None)
+
+    # check that all required parameters were passed
+    if any(pased_params[key] is None for key in required_args):
+        response['message'] = "not all parameters were passed"
+    else:
+        try:
+            passed_params['options'] = ast.literal_eval(passed_params['options'])
+        except:
+            response['message'] = 'options were not passed correctly'
+        else:
+            user = User.query.filter_by(uuid=passed_params['uuid']).first()
+            post = Post(passed_params['question'], passed_params['lat'], passed_params['lng'], user)
+            db.session.add(post)
+            db.session.commit()
+        return json.dumps(response)
+
+@app.route('/test')
+def test():
+    user = User.query.filter_by(uuid='hri1o2jd-uto1-74jd-pqjfoe1g0315').first()
+    print(user)
+    print(user.posts.all())
+    print(request.form)
+    return 'test'
