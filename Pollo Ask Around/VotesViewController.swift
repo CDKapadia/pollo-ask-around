d45ebc67407fd41fcbe4381c5f34b655e56ba4f0
Chiraag Kapadia <Chiraag@chiraags-mbp.dhcp.wustl.edu>
2016-11-30 23:21:34 -0600

    Buttons options

--- Pollo Ask Around/VotesViewController.swift
+++ Pollo Ask Around/VotesViewController.swift
@@ -11,12 +11,35 @@ import UIKit
 class VotesViewController: UIViewController {
     
     @IBOutlet weak var titleOfPoll: UILabel!
+    @IBOutlet weak var optionsStackView: UIStackView!
     
-    
+    //fill this with options.
+    var myArray = [String()]
     var pollTitle = String()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //optionsStackView.translatesAutoresizingMaskIntoConstraints = false;
         // Do any additional setup after loading the view.
+        var tag = 1
+        for name in myArray{
+            let button = UIButton()
+            //button.backgroundColor = .orange
+            button.setTitle(name, for: .normal)
+            button.setTitleColor(.black, for: .normal)
+            button.titleLabel?.adjustsFontSizeToFitWidth = true
+            button.titleLabel!.numberOfLines = 1
+           
+            //maybe make a custon button
+            button.tag = tag
+            tag+=1
+            button.addTarget(self, action: #selector(optionChosen), for: .touchUpInside)
+            optionsStackView.addArrangedSubview(button)
+        }
+
+        
+    
     }
     override func viewWillAppear(_ animated: Bool) {
        titleOfPoll.text = pollTitle
@@ -26,6 +49,10 @@ class VotesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func optionChosen(sender: UIButton!) {
+        
+        print("Button ", sender.tag ," tapped")
+    }
 
     /*
     // MARK: - Navigation
